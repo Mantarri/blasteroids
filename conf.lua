47dcd2d3402247168fadd6426e0bdbdb2e031eba
Mistery Man <themisteryman2@outlook.com>
2019-04-27 20:45:35 -0700

    Add version to 'conf.lua'

--- conf.lua
+++ conf.lua
@@ -1,5 +1,6 @@
 function love.conf(t)
 	love.filesystem.setIdentity("Blasteroids")
+	t.version = "11.2"
 	t.window.width = 800
 	t.window.height = 600
 	t.window.title = "Blasteroids " .. t.window.width .. "x" .. t.window.height
